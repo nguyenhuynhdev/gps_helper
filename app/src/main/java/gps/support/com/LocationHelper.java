c9c284d9578a68b30c3e22b9f0461d9890c3f0d7
Nguyen <nguyenhuynhdev@gmail.com>
2018-10-09 11:32:31 +0700

    Location helper

--- app/src/main/java/gps/support/com/LocationHelper.java
+++ app/src/main/java/gps/support/com/LocationHelper.java
@@ -2,16 +2,18 @@ package gps.support.com;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
 
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
@@ -24,55 +26,51 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.Serializable;
 
-/**
- * Support class to get current location
- * implements [{@link LocationListener}] to handle location state, {@link Runnable} for create
- * time out
- */
-class GpsHelper implements LocationListener, Runnable {
+public class LocationHelper implements LocationListener, Runnable {
 
-    //Control time out
-    private Handler handler;
-    private Context mContext;
-    @SuppressLint("StaticFieldLeak")
-    private static GpsHelper mGpsHelper;
+    private static final int LOCATION_PERMISSION = 4321;
+
+    private final Handler handler;
+    private long timeOut = Long.MAX_VALUE;
+    private static LocationHelper locationHelper;
+    private final LocationManager locationManager;
     private LocationCallBack mCallBack;
-    private LocationManager locationManager;
 
-    @MainThread
-    static GpsHelper getInstance(Context context) {
-        if (mGpsHelper != null) {
-            return mGpsHelper;
-        } else {
-            mGpsHelper = new GpsHelper(context.getApplicationContext());
-            return mGpsHelper;
-        }
+    public static LocationHelper getInstance(Context context) {
+        return locationHelper != null ? locationHelper :
+                (locationHelper = new LocationHelper(context));
     }
 
-    /**
-     * private Constructor
-     *
-     * @param context a applicationContext needed
-     */
-    private GpsHelper(Context context) {
-        mContext = context;
+    private LocationHelper(Context context) {
         handler = new Handler(Looper.getMainLooper());
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
     }
 
-    /**
-     * Get current location with time out
-     *
-     * @param timeOut  Time out when waiting [{@link LocationCallBack}]
-     * @param callBack [{@link LocationCallBack}] response {@link Task}
-     */
-    @SuppressWarnings("SameParameterValue")
-    @SuppressLint("MissingPermission")
-    @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION})
-    synchronized void getCurrentLocation(long timeOut, final LocationCallBack callBack) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull int[] grantResults, Activity activity) {
+        if (requestCode == LOCATION_PERMISSION) {
+            if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestLocation(activity, mCallBack);
+                return;
+            }
+        }
+        mCallBack.onResponse(new Task(null, new Throwable("Permission denied"), false));
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Activity activity) {
+        if (requestCode == LOCATION_PERMISSION) {
+            if (resultCode == -1) {
+                requestLocation(activity, mCallBack);
+            } else {
+                mCallBack.onResponse(new Task(null, new Throwable("Permission denied"), false));
+            }
+
+        }
+    }
+
+    public synchronized void requestLocation(final Activity activity, LocationCallBack locationCallBack) {
         try {
-            mCallBack = callBack;
+            mCallBack = locationCallBack;
+            handler.removeCallbacks(this);
             handler.postDelayed(this, timeOut);
             //check gps everyTime call this function
             LocationRequest locationRequest = new LocationRequest();
@@ -82,31 +80,47 @@ class GpsHelper implements LocationListener, Runnable {
 
             LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
             builder.addLocationRequest(locationRequest);
-            SettingsClient client = LocationServices.getSettingsClient(mContext);
+            SettingsClient client = LocationServices.getSettingsClient(activity);
             com.google.android.gms.tasks.Task<LocationSettingsResponse> task =
                     client.checkLocationSettings(builder.build());
             task.addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
                     if (e instanceof ResolvableApiException) {
-                        handler.removeCallbacks(GpsHelper.this);
-                        mCallBack.onResponse(new Task(null, e, false));
+                        handler.removeCallbacks(LocationHelper.this);
+                        try {
+                            ((ResolvableApiException) e).startResolutionForResult(activity, LOCATION_PERMISSION);
+                        } catch (IntentSender.SendIntentException e1) {
+                            mCallBack.onResponse(new Task(null, e1, false));
+                        }
                     }
                 }
             });
             task.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
                 @Override
                 public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        String[] LOCATION_PERMS = {
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        };
+                        ActivityCompat.requestPermissions(activity, LOCATION_PERMS, LOCATION_PERMISSION);
+                        return;
+                    }
                     locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                            1, 0, GpsHelper.this);
+                            1, 0, LocationHelper.this);
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                            1, 0, GpsHelper.this);
+                            1, 0, LocationHelper.this);
                 }
             });
         } finally {
             locationManager.removeUpdates(this);
         }
+    }
 
+    public LocationHelper setTimeOut(long timeOut) {
+        this.timeOut = timeOut;
+        return this;
     }
 
     @Override
@@ -127,9 +141,9 @@ class GpsHelper implements LocationListener, Runnable {
         //Reset listener
         locationManager.removeUpdates(this);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                1, 0, GpsHelper.this);
+                1, 0, LocationHelper.this);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                1, 0, GpsHelper.this);
+                1, 0, LocationHelper.this);
     }
 
     @Override
